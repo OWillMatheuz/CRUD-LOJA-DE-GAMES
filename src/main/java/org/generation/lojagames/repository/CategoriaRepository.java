c11806762fa452fd8f3f4b41795279dbf640f509
Will Matheus <119905167+OWillMatheuz@users.noreply.github.com>
2023-02-01 13:39:34 -0300

    Update CategoriaRepository.java

--- src/main/java/org/generation/lojagames/repository/CategoriaRepository.java
+++ src/main/java/org/generation/lojagames/repository/CategoriaRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.generation.lojagames.model.CategoriaGames;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaGames, Long> {
 	public List<CategoriaGames> findAllByComputadorContainingIgnoreCase
 	(@Param("computador") String computador);
